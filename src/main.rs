deaf3dd9cb21f5367f397b42731c750f9b79e6ec
Paul Cavallaro <paulcavallaro@gmail.com>
2015-03-30 22:21:41 -0400

    Add simple functions

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 mod types;
 
-use types::{Value, Expr, eval};
+use types::{Value, Expr, Context, Func_, eval};
+use std::collections::HashMap;
 
 fn main() {
+  let ctx = Context { vars : HashMap::new() };
   let a = Expr::Val(Value::Int64(64));
   let b = Expr::Val(Value::Int64(65));
   let c = Expr::Print(Box::new(Expr::Plus(Box::new(a), Box::new(b))));
-  let d = eval(c);
-  let _e = eval(Expr::Print(Box::new(Expr::Val(d))));
+  let d = eval(&ctx, c);
+  let _e = eval(&ctx, Expr::Print(Box::new(Expr::Val(d))));
 
   let f = Expr::Val(Value::Str("Hello, World!".to_string()));
-  let g = eval(Expr::Print(Box::new(f)));
-  let _h = eval(Expr::Print(Box::new(Expr::Val(g))));
+  let g = eval(&ctx, Expr::Print(Box::new(f)));
+  let _h = eval(&ctx, Expr::Print(Box::new(Expr::Val(g))));
+
+  let body = Expr::Plus(Box::new(Expr::Var("i".to_string())),
+                        Box::new(Expr::Val(Value::Int64(1))));
+  let inc = Func_ { name : "inc".to_string(),
+                    params : vec!["i".to_string()],
+                    body : Box::new(body),
+                  };
+  let app = Expr::App(inc, vec![Expr::Val(Value::Int64(20))]);
+  let res = eval(&ctx, app);
+  let _ = eval(&ctx, Expr::Print(Box::new(Expr::Val(res))));
 
   let a = Expr::Val(Value::Int64(64));
   let f = Expr::Val(Value::Str("Hello, World!".to_string()));
-  let i = eval(Expr::Plus(Box::new(a), Box::new(f)));
-  let _ = eval(Expr::Print(Box::new(Expr::Val(i))));
+  let i = eval(&ctx, Expr::Plus(Box::new(a), Box::new(f)));
+  let _ = eval(&ctx, Expr::Print(Box::new(Expr::Val(i))));
 }
--- src/types.rs
+++ src/types.rs
@@ -1,18 +1,36 @@
 extern crate core;
 
 use self::core::fmt;
+use std::collections::HashMap;
 
+#[derive(Clone)]
 pub enum Value {
   Int64 (i64),
   Str (String),
+  Func (Func_),
   Unit,
 }
 
+#[derive(Clone)]
+pub struct Context {
+  pub vars : HashMap<String, Expr>,
+}
+
+#[derive(Clone)]
+pub struct Func_ {
+  pub name : String,
+  pub params : Vec<String>,
+  pub body : Box<Expr>,
+}
+
+#[derive(Clone)]
 pub enum Expr {
   Val (Value),
+  Var (String),
   // Builtins
   Plus (Box<Expr>, Box<Expr>),
   Print (Box<Expr>),
+  App (Func_, Vec<Expr>),
 }
 
 impl fmt::Display for Value {
@@ -24,22 +42,31 @@ impl fmt::Display for Value {
       &Value::Str(ref str) => {
         str.fmt(formatter)
       },
+      &Value::Func(ref f) => {
+        write!(formatter, "Function {} of arity {}", f.name, f.params.len())
+      },
       &Value::Unit =>
       {
-        write!(formatter, "()");
-        Ok(())
+        write!(formatter, "()")
       }
     }
   }
 }
 
-pub fn eval(expr : Expr) -> Value {
+pub fn eval(ctx : &Context, expr : Expr) -> Value {
   match expr {
     Expr::Val(v) => v,
+    Expr::Var(ref id) =>
+    {
+      match ctx.vars.get(id) {
+        None => panic!("Unbound name {}", id),
+        Some(e) => eval(ctx, e.clone()),
+      }
+    },
     Expr::Plus(e1, e2) =>
     {
-      let r1 = eval(*e1);
-      let r2 = eval(*e2);
+      let r1 = eval(ctx, *e1);
+      let r2 = eval(ctx, *e2);
       match (r1, r2) {
         (Value::Int64(i1), Value::Int64(i2)) => Value::Int64(i1+i2),
         (l, r) =>
@@ -50,24 +77,21 @@ pub fn eval(expr : Expr) -> Value {
     },
     Expr::Print(e) =>
     {
-      let res = eval(*e);
-      match res {
-        Value::Int64(i) =>
-        {
-          println!("{}", i);
-          Value::Unit
-        },
-        Value::Str(str) =>
-        {
-          println!("{}", str);
-          Value::Unit
-        }
-        Value::Unit =>
-        {
-          println!("()");
-          Value::Unit
-        }
+      let res = eval(ctx, *e);
+      println!("{}", res);
+      Value::Unit
+    },
+    Expr::App(f, args) =>
+    {
+      let mut new_ctx = ctx.clone();
+      if f.params.len() != args.len() {
+        panic!("Partial application of function {} of arity {} to {} args",
+               f.name, f.params.len(), args.len())
+      };
+      for (param, arg) in f.params.iter().zip(args.iter()) {
+        new_ctx.vars.insert(param.clone(), arg.clone());
       }
-    }
+      eval(&new_ctx, *f.body)
+    },
   }
 }
